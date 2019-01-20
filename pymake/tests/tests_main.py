07b8f3dd82330cff093c7c17937a250d299e8a14
Casper da Costa-Luis <casper.dcl@physics.org>
2019-01-20 17:26:37 +0000

    split and tidy tests

--- pymake/tests/tests_main.py
+++ pymake/tests/tests_main.py
@@ -4,18 +4,18 @@ from os import path
 from textwrap import dedent
 from pymake import main, PymakeKeyError, PymakeTypeError
 
+dn = path.dirname
+fname = path.join(dn(dn(dn(path.abspath(__file__)))),
+                  "examples", "Makefile").replace('\\', '/')
+
 
 def _sh(*cmd, **kwargs):
     return subprocess.Popen(cmd, stdout=subprocess.PIPE,
                             **kwargs).communicate()[0].decode('utf-8')
 
 
-# WARNING: this should be the last test as it messes with sys.stdin, argv
 def test_main():
     """Test execution"""
-    dn = path.dirname
-    fname = path.join(dn(dn(dn(path.abspath(__file__)))),
-                      "examples", "Makefile").replace('\\', '/')
     res = _sh(sys.executable, '-c', dedent('''\
               from pymake import main; import sys
               sys.argv = ["", "-f", "''' + fname + '''"]
@@ -27,12 +27,15 @@ def test_main():
     assert ("hello world" in res)
 
     # semi-fake test which gets coverage:
-    _SYS = sys.stdin, sys.argv
-
+    _SYS = sys.argv
     sys.argv = ['', '-f', fname]
     main()
+    sys.argv = _SYS
 
+
+def test_invalid_alias():
     """Test invalid alias"""
+    _SYS = sys.argv
     sys.argv = ['', '-f', fname, 'foo']
     try:
         main()
@@ -41,21 +44,37 @@ def test_main():
             raise
     else:
         raise PymakeKeyError('foo')
+    sys.argv = _SYS
 
+
+def test_multi_target():
     """Test various targets"""
+    _SYS = sys.argv
     for trg in ['circle', 'empty', 'one']:
         sys.argv = ['', '-s', '-f', fname, trg]
         main()
+    sys.argv = _SYS
 
+
+def test_print_data_base():
     """Test --print-data-base with errors"""
+    _SYS = sys.argv
     sys.argv = ['', '-s', '-p', '-f', fname, 'err']
     main()
+    sys.argv = _SYS
 
+
+def test_just_print():
     """Test --just-print with errors"""
+    _SYS = sys.argv
     sys.argv = ['', '-s', '-n', '-f', fname, 'err']
     main()
+    sys.argv = _SYS
 
+
+def test_ignore_errors():
     """Test --ignore-errors"""
+    _SYS = sys.argv
     sys.argv = ['', '-s', '-f', fname, 'err']
     try:
         main()
@@ -66,14 +85,16 @@ def test_main():
 
     sys.argv = ['', '-s', '-i', '-f', fname, 'err']
     main()
+    sys.argv = _SYS
 
+
+def test_help_version():
     """Test help and version"""
+    _SYS = sys.argv
     for i in ('-h', '--help', '-v', '--version'):
         sys.argv = ['', i]
         try:
             main()
         except SystemExit:
             pass
-
-    # clean up
-    sys.stdin, sys.argv = _SYS
+    sys.argv = _SYS
